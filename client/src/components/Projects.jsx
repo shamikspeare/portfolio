f304ebbfefe51a315e5421f645a8a46c57ec5e69
shamikspeare <gshamik14@gmail.com>
2025-12-13 12:58:43 +0530

    feat: Initialize portfolio redesign branch

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,25 +1,22 @@
-import React from 'react'
-import MystreamCard from './MystreamCard'
+import React from "react";
+import MystreamCard from "./MystreamCard";
 import { Highlighter } from "@/components/magicui/highlighter";
-
 
 const Projects = () => {
   return (
     <>
       <div className="mt-14 font-goldman text-5xl sm:text-6xl lg:text-[100px] ">
-        <Highlighter action="highlight" color="hsl(198.4 93.2% 59.6%)" isView={true}>
+        <Highlighter
+          action="highlight"
+          color="hsl(198.4 93.2% 59.6%)"
+          isView={true}
+        >
           Projects
         </Highlighter>
       </div>
-      <MystreamCard/>
-            
-            
-       
-       
+      <MystreamCard />
     </>
+  );
+};
 
-
-  )
-}
-
-export default Projects
+export default Projects;
